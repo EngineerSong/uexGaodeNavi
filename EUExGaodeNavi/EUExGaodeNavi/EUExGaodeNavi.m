f0a6800197e702105b98eaea58f5035a3c25ec5e
connyliu <21165491@qq.com>
2015-12-30 11:46:48 +0800

    3.0.0

--- EUExGaodeNavi/EUExGaodeNavi/EUExGaodeNavi.m
+++ EUExGaodeNavi/EUExGaodeNavi/EUExGaodeNavi.m
@@ -23,9 +23,23 @@
 
 #import "EUExGaodeNavi.h"
 #import "EUtility.h"
+#import "JSON.h"
+#import "uexGaodeNaviManager.h"
+
+
+@interface EUExGaodeNavi()<AMapNaviManagerDelegate,AMapNaviViewControllerDelegate>
+
+@property (nonatomic,weak)AMapNaviManager *manager;
+@property (nonatomic,strong)MAMapView *mapView;
+@property (nonatomic,strong)AMapNaviViewController *naviController;
+@property (nonatomic,assign)BOOL useGPSNavi;
+@end
 
 
 @implementation EUExGaodeNavi
+
+
+
 
 #pragma mark - Life Cycle
 
@@ -38,11 +52,257 @@
 }
 
 - (void)clean{
-    
+    _mapView=nil;
+    _naviController=nil;
 }
 
 - (void)dealloc{
     [self clean];
+}
+
+- (MAMapView *)mapView{
+    if(!_mapView){
+        _mapView=[[MAMapView alloc]init];
+    }
+    return _mapView;
+}
+
+- (AMapNaviViewController *)naviController{
+    if(!_naviController){
+        _naviController=[[AMapNaviViewController alloc]initWithMapView:self.mapView delegate:self];
+    }
+    return _naviController;
+}
+
+#pragma mark - API
+
+- (void)init:(NSMutableArray *)inArguments{
+    NSString *appKey = [self appKeyFromInit:inArguments];
+    if(!appKey || ![appKey isKindOfClass:[NSString class]] || appKey.length == 0){
+        appKey=[[NSBundle mainBundle]infoDictionary][@"uexGaodeNaviAppKey"];
+    }
+    [AMapNaviServices sharedServices].apiKey=appKey;
+    [MAMapServices sharedServices].apiKey=appKey;
+    self.manager=[uexGaodeNaviManager defaultManager].naviManager;
+    self.manager.delegate=self;
+    [self callbackJSONWithFunction:@"cbInit" object:nil];
+}
+
+
+- (void)calculateWalkRoute:(NSMutableArray *)inArguments{
+    if([inArguments count] < 1){
+        [self cbCalculateRouteWithResult:NO];
+        return;
+    }
+    id info = [inArguments[0] JSONValue];
+    if(!info || ![info isKindOfClass:[NSDictionary class]]){
+        [self cbCalculateRouteWithResult:NO];
+        return;
+    }
+    
+    AMapNaviPoint *endPoint = [self pointFromArray:info[@"endPoint"]];
+    if(!endPoint){
+        [self cbCalculateRouteWithResult:NO];
+        return;
+    }
+    AMapNaviPoint *startPoint = [self pointFromArray:info[@"startPoint"]];
+    BOOL isSuccess;
+    if(!startPoint){
+        isSuccess = [self.manager calculateWalkRouteWithEndPoints:@[endPoint]];
+    }else{
+        isSuccess = [self.manager calculateWalkRouteWithStartPoints:@[startPoint] endPoints:@[endPoint]];
+    }
+    [self cbCalculateRouteWithResult:isSuccess];
+}
+
+- (void)calculateDriveRoute:(NSMutableArray *)inArguments{
+    if([inArguments count] < 1){
+        return;
+    }
+    id info = [inArguments[0] JSONValue];
+    if(!info || ![info isKindOfClass:[NSDictionary class]]){
+        return;
+    }
+    //endPoints
+    NSArray *endPoints=[info[@"endPoints"] isKindOfClass:[NSArray class]]?[self pointsFromArray:info[@"endPoints"]]:nil;
+    if(!endPoints){
+        AMapNaviPoint *endPoint = [self pointFromArray:info[@"endPoint"]];
+        if(!endPoint){
+            [self cbCalculateRouteWithResult:NO];
+            return;
+        }
+        endPoints=@[endPoint];
+    }
+    //startPoints
+    NSArray *startPoints=[info[@"startPoints"] isKindOfClass:[NSArray class]]?[self pointsFromArray:info[@"startPoints"]]:nil;
+    if(!startPoints){
+        AMapNaviPoint *startPoint = [self pointFromArray:info[@"startPoint"]];
+        if(startPoint){
+            startPoints=@[startPoint];
+        }
+        
+    }
+    //wayPoints
+    NSArray *wayPoints=[info[@"wayPoints"] isKindOfClass:[NSArray class]]?[self pointsFromArray:info[@"wayPoints"]]:nil;
+    
+    //driveMode
+    NSInteger driveMode=[info[@"driveMode"] integerValue]?:0;
+    AMapNaviDrivingStrategy strategy;
+    switch (driveMode) {
+        case 1:{
+            strategy=AMapNaviDrivingStrategySaveMoney;
+            break;
+        }
+        case 2:{
+            strategy=AMapNaviDrivingStrategyShortDistance;
+            break;
+        }
+        case 3:{
+            strategy=AMapNaviDrivingStrategyNoExpressways;
+            break;
+        }
+        case 4:{
+            strategy=AMapNaviDrivingStrategyFastestTime;
+            break;
+        }
+        case 5:{
+            strategy=AMapNaviDrivingStrategyAvoidCongestion;
+            break;
+        }
+        default:{
+            strategy=AMapNaviDrivingStrategyDefault;
+            break;
+        }
+    }
+    
+    
+    BOOL isSuccess;
+    if(!startPoints){
+        isSuccess = [self.manager calculateDriveRouteWithEndPoints:endPoints wayPoints:wayPoints drivingStrategy:strategy];
+    }else{
+        isSuccess = [self.manager calculateDriveRouteWithStartPoints:startPoints endPoints:endPoints wayPoints:wayPoints drivingStrategy:strategy];
+    }
+    [self cbCalculateRouteWithResult:isSuccess];
+
+}
+
+- (void)startNavi:(NSMutableArray *)inArguments{
+    self.useGPSNavi=YES;
+    if(inArguments.count>0){
+        id info=[inArguments[0] JSONValue];
+        if (info && [info isKindOfClass:[NSDictionary class]] && info[@"type"]){
+            self.useGPSNavi=!([info[@"type"] integerValue] == 1);
+        }
+    }
+    //[EUtility brwView:self.meBrwView presentModalViewController:self.naviController animated:YES];
+    [self.manager presentNaviViewController:self.naviController animated:YES];
+}
+
+
+- (void)stopNavi:(NSMutableArray *)inArguments{
+    if(!_naviController){
+        return;
+    }
+    [self.manager stopNavi];
+    [self.manager dismissNaviViewControllerAnimated:YES];
+
+}
+
+#pragma mark - Delegate
+
+
+
+- (void)naviManager:(AMapNaviManager *)naviManager didPresentNaviViewController:(UIViewController *)naviViewController{
+    if(self.useGPSNavi){
+        [self.manager startGPSNavi];
+    }else{
+        [self.manager startEmulatorNavi];
+    }
+    BOOL iOS9=[[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0;
+    NSArray *backgroundModes=[[NSBundle mainBundle]infoDictionary][@"UIBackgroundModes"];
+    BOOL requireBackgroundLocationUpdate = backgroundModes && [backgroundModes isKindOfClass:[NSArray class]] && [backgroundModes containsObject:@"location"];
+    if (iOS9 && requireBackgroundLocationUpdate) {
+        self.manager.allowsBackgroundLocationUpdates = YES;
+    }
+}
+- (void)naviManager:(AMapNaviManager *)naviManager didDismissNaviViewController:(UIViewController *)naviViewController{
+    [self clean];
+}
+- (void)naviViewControllerCloseButtonClicked:(AMapNaviViewController *)naviViewController
+{
+    
+    [self.manager stopNavi];
+    [self.manager dismissNaviViewControllerAnimated:YES];
+}
+
+- (void)naviManager:(AMapNaviManager *)naviManager playNaviSoundString:(NSString *)soundString soundStringType:(AMapNaviSoundType)soundStringType
+{
+    NSMutableDictionary *dict=[NSMutableDictionary dictionary];
+    [dict setValue:soundString forKey:@"text"];
+    
+    [self callbackJSONWithFunction:@"onGetNavigationText" object:dict];
+
+}
+
+- (void)naviManagerNeedRecalculateRouteForYaw:(AMapNaviManager *)naviManager{
+    [self callbackJSONWithFunction:@"onReCalculateRouteForYaw" object:nil];
+}
+
+- (void)naviManager:(AMapNaviManager *)naviManager didStartNavi:(AMapNaviMode)naviMode{
+    [self callbackJSONWithFunction:@"onStartNavi" object:nil];
+}
+- (void)naviManagerDidEndEmulatorNavi:(AMapNaviManager *)naviManager{
+    [self callbackJSONWithFunction:@"onArriveDestination" object:nil];
+}
+
+- (void)naviManagerOnArrivedDestination:(AMapNaviManager *)naviManager{
+    [self callbackJSONWithFunction:@"onArriveDestination" object:nil];
+}
+#pragma mark - Private
+
+
+
+
+- (void)cbCalculateRouteWithResult:(BOOL)isSuccess{
+    [self callbackJSONWithFunction:@"cbCalculateRoute" object:@{@"result":@(isSuccess)}];
+}
+
+
+- (NSString *)appKeyFromInit:(NSMutableArray *)inArguments{
+    if([inArguments count] < 1){
+        return nil;
+    }
+    id info = [inArguments[0] JSONValue];
+    if(!info || ![info isKindOfClass:[NSDictionary class]]){
+        return nil;
+    }
+    return info[@"appKey"];
+}
+
+
+
+- (AMapNaviPoint *)pointFromArray:(NSArray *)array{
+    if(!array || ![array isKindOfClass:[NSArray class]]){
+        return nil;
+    }
+    if(array.count < 2){
+        return nil;
+    }
+    return [AMapNaviPoint locationWithLatitude:[array[0] floatValue] longitude:[array[1] floatValue]];
+}
+
+- (NSArray<AMapNaviPoint *> *)pointsFromArray:(NSArray *)array{
+    if(!array || ![array isKindOfClass:[NSArray class]]){
+        return nil;
+    }
+    NSMutableArray *result=[NSMutableArray array];
+    for(NSArray *aPointArray in array){
+        AMapNaviPoint *point=[self pointFromArray:aPointArray];
+        if(point){
+            [result addObject:point];
+        }
+    }
+    return result;
 }
 
 #pragma mark - JSON Callback
